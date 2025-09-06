bf9b65d933fa4e3260984f28553982d064f8a76d
hay-oon <hayooncode@gmail.com>
2025-09-06 12:38:23 +0900

    chore: LoginRequest에서 불필요한 로그인 타입 필드 제거

--- src/main/java/com/brs/gridge/controller/dto/LoginRequest.java
+++ src/main/java/com/brs/gridge/controller/dto/LoginRequest.java
@@ -1,8 +1,6 @@
 package com.brs.gridge.controller.dto;
 
-import com.brs.gridge.domain.vo.LoginType;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -14,7 +12,4 @@ public class LoginRequest {
     
     @NotBlank(message = "비밀번호는 필수입니다")
     private String password;
-    
-    @NotNull(message = "로그인 타입은 필수입니다")
-    private LoginType loginType;
 }
