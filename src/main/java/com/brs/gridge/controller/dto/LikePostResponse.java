08c2a55991f2ddf68b98a422f4e27b1e9c4d9a79
hay-oon <hayooncode@gmail.com>
2025-09-06 12:23:35 +0900

    fix: BookmarkPostResponse 및 LikePostResponse 클래스에 @JsonProperty 어노테이션 추가하여 JSON 직렬화 시 필드 이름 지정

--- src/main/java/com/brs/gridge/controller/dto/LikePostResponse.java
+++ src/main/java/com/brs/gridge/controller/dto/LikePostResponse.java
@@ -1,5 +1,7 @@
 package com.brs.gridge.controller.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
@@ -11,6 +13,7 @@ public class LikePostResponse {
     
     private boolean success;
     private String message;
+    @JsonProperty("liked") // 자바의 boolean getter 네이밍 규칙 때문에 isLiked → liked로 자동 변환되므로 @JsonProperty로 명시적으로 지정
     private boolean isLiked;
     private long likeCount;
     
